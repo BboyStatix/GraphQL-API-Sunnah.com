956b48d76ed41c1646160384d1973156fd36c7a8
Naveed Elahi <statix231@hotmail.com>
2021-05-08 16:33:39 +0800

    Move resolvers to their own file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { ApolloServer, gql } = require("apollo-server");
 const HadithApi = require("./HadithApi");
+const resolvers = require("./resolvers");
 
 const typeDefs = gql`
   enum CollectionName {
@@ -102,59 +103,6 @@ const typeDefs = gql`
     getHadithByUrn(urn: Int!): Hadith
   }
 `;
-
-const resolvers = {
-  Query: {
-    Collections: async (_, __, { dataSources }) => {
-      const response = await dataSources.hadithApi.getCollections();
-      return response.data;
-    },
-    Collection: async (_, { name }, { dataSources }) => {
-      return dataSources.hadithApi.getCollection(name);
-    },
-    Chapters: async (_, { collectionName, bookNumber }, { dataSources }) => {
-      const response = await dataSources.hadithApi.getChapters(
-        collectionName,
-        bookNumber
-      );
-      return response.data;
-    },
-    getHadithsByCollectionAndBookNumber: async (
-      _,
-      { collectionName, bookNumber },
-      { dataSources }
-    ) => {
-      const response = await dataSources.hadithApi.getHadithsByCollectionAndBookNumber(
-        collectionName,
-        bookNumber
-      );
-      return response.data;
-    },
-    Books: async (_, { collectionName }, { dataSources }) => {
-      const response = await dataSources.hadithApi.getBooks(collectionName);
-      return response.data;
-    },
-    Book: async (_, { collectionName, bookNumber }, { dataSources }) => {
-      return dataSources.hadithApi.getBook(collectionName, bookNumber);
-    },
-    getHadithByCollectionAndHadithNumber: async (
-      _,
-      { collectionName, hadithNumber },
-      { dataSources }
-    ) => {
-      return dataSources.hadithApi.getHadithByCollectionAndHadithNumber(
-        collectionName,
-        hadithNumber
-      );
-    },
-    getRandomHadith: async (_, __, { dataSources }) => {
-      return dataSources.hadithApi.getRandomHadith();
-    },
-    getHadithByUrn: async (_, { urn }, { dataSources }) => {
-      return dataSources.hadithApi.getHadithByUrn(urn);
-    },
-  },
-};
 
 const server = new ApolloServer({
   typeDefs,
---
+++ resolvers.js
@@ -0,0 +1,54 @@
+const resolvers = {
+  Query: {
+    Collections: async (_, __, { dataSources }) => {
+      const response = await dataSources.hadithApi.getCollections();
+      return response.data;
+    },
+    Collection: async (_, { name }, { dataSources }) => {
+      return dataSources.hadithApi.getCollection(name);
+    },
+    Chapters: async (_, { collectionName, bookNumber }, { dataSources }) => {
+      const response = await dataSources.hadithApi.getChapters(
+        collectionName,
+        bookNumber
+      );
+      return response.data;
+    },
+    getHadithsByCollectionAndBookNumber: async (
+      _,
+      { collectionName, bookNumber },
+      { dataSources }
+    ) => {
+      const response = await dataSources.hadithApi.getHadithsByCollectionAndBookNumber(
+        collectionName,
+        bookNumber
+      );
+      return response.data;
+    },
+    Books: async (_, { collectionName }, { dataSources }) => {
+      const response = await dataSources.hadithApi.getBooks(collectionName);
+      return response.data;
+    },
+    Book: async (_, { collectionName, bookNumber }, { dataSources }) => {
+      return dataSources.hadithApi.getBook(collectionName, bookNumber);
+    },
+    getHadithByCollectionAndHadithNumber: async (
+      _,
+      { collectionName, hadithNumber },
+      { dataSources }
+    ) => {
+      return dataSources.hadithApi.getHadithByCollectionAndHadithNumber(
+        collectionName,
+        hadithNumber
+      );
+    },
+    getRandomHadith: async (_, __, { dataSources }) => {
+      return dataSources.hadithApi.getRandomHadith();
+    },
+    getHadithByUrn: async (_, { urn }, { dataSources }) => {
+      return dataSources.hadithApi.getHadithByUrn(urn);
+    },
+  },
+};
+
+module.exports = resolvers;
